8bd9778abd459ea25c281aab5bf3ccb073a71974
AnhKhoa1405 <truonganhkhoa1405@gmail.com>
2021-04-21 23:44:00 +0700

    First Commit

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-const test = [{
-        name: "Item 1",
+const navItem = [{
+        name: "Item1",
+        children: [
+            "Item 1.1",
+            "Item 1.2",
+            "Item 1.3",
+        ]
     },
     {
-        name: "Item 2",
+        name: "Item2",
+        children: [
+            "Item 2.1",
+            "Item 2.2",
+            "Item 2.3",
+            "Item 2.4",
+            "Item 2.5",
+        ]
     },
     {
-        name: "Item 3",
+        name: "Item3",
     },
     {
-        name: "Item 4",
+        name: "Item4",
     },
     {
-        name: "Item 5",
+        name: "Item5",
     },
-    {
-        name: "Item 6",
-    },
-];
+]
 
 window.onload = function() {
-    for (let i = 0; i < test.length; i++) {
+    for (let i = 0; i < navItem.length; i++) {
         document.getElementById("renderMenu").insertAdjacentHTML("beforeend",
-            `<span>${test[i].name}</span>`)
+                `<span id="${navItem[i].name}" onmouseover=addChildern('${navItem[i].name}')>${navItem[i].name}</span>`)
+            // if (navItem[i].children != undefined) {
+            //     for (let j = 0; j < navItem[i].children.length; j++) {
+            //         document.getElementById(`${navItem[i].name}`).insertAdjacentHTML("beforeend",
+            //             `<span id="${navItem[i].children[j]}")>${navItem[i].children[j]}</span>`)
+            //     }
+
+        // }
     }
+}
+
+function addChildern(name) {
+
 }
